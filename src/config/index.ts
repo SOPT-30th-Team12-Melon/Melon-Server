6d9e9ca28a6973be7f20d817f8ebcc8077525d58
boong_u <boong_u@incross.com>
2022-05-26 20:36:56 +0900

    Hot: elasticbeanstalk 환경변수 설정

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,23 +1,23 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv'
 
 // Set the NODE_ENV to 'development' by default
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-const envFound = dotenv.config();
-if (envFound.error) {
+const envFound = dotenv.config()
+if (process.env.NODE_ENV === 'local' && envFound.error) {
   // This error should crash whole process
 
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  throw new Error("⚠️  Couldn't find .env file  ⚠️")
 }
 
 export default {
   /**
    * Your favorite port
    */
-  port: parseInt(process.env.PORT as string, 10) as number,
+  port: (parseInt(process.env.PORT as string, 10) as number) || 5000,
 
   /**
    * MongoDB URI
    */
-  mongoURI: process.env.MONGODB_URI as string,
-};
+  mongoURI: process.env.MONGODB_URI as string
+}
